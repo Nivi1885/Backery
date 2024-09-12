22cf84b4aeaa458bbeb989bfc831b95dc676fcee
Nivi1885 <146195626+Nivi1885@users.noreply.github.com>
2024-09-12 15:48:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let navbar = document.querySelector('.header .navbar');
+let menu = document.querySelector('#menu-btn');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+let cart = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cart.classList.add('active');
+}
+
+document.querySelector('#close-form').onclick = () =>{
+    cart.classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+    grabCursor:true,
+    loop:true,
+    cnteredSlides:true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+function log(){
+    var name = document.querySelector("#name").value;
+    var pwd = document.querySelector("#pwd").value;
+
+    if(name==""){
+        alert("username is required");
+    } else if(pwd == ""){
+        alert("password is empty");
+    }
+}
